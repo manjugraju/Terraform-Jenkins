6192e6c0b139199527a4f4ce18a7771d54031613
manjugraju <manjugraju@gmail.com>
2024-06-27 01:00:38 +0530

    attacked keypair

--- Terraform-aws-infra/main.tf
+++ Terraform-aws-infra/main.tf
@@ -2,9 +2,15 @@ provider "aws" {
     region = "us-eaast-1"
 }
 
+variable "existing_key_pair" {
+    type = string
+    default = "test-keypair"
+}
 resource "aws_instance" "web-server-man" {
     ami = "ami-08a0d1e16fc3f61ea"
     instance_type = "t2.micro"
+    key_name = var.existing_key_pair
+
     tags = {
       name = "web-server-man"
     }
